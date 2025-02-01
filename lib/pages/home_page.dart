1c96755b7d0c50af8e1a85f494f2b2abc881f934
Krane <freesme@163.com>
2025-02-01 19:24:39 +0800

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -38,12 +38,41 @@ class _HomePageState extends State<HomePage> {
                 control: const SwiperControl(),
               ),
             ),
-            Expanded(
-                child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      return _listItemView();
-                    },
-                    itemCount: 10))
+            // Expanded(
+            //     child: ListView.builder(
+            //         itemBuilder: (context, index) {
+            //           return _layout();
+            //         },
+            //         itemCount: 10))
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text('Center'),
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              padding: EdgeInsets.all(18.0),
+              child: Text('Container padding 18.0'),
+            ),
+            Center(
+                child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      // 分别设置四个角的圆角半径
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        // topRight: Radius.circular(20),
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(40),
+                      ),
+                    )))
           ],
         )));
   }
@@ -57,12 +86,18 @@ class _HomePageState extends State<HomePage> {
             height: 60,
             width: 30,
           ),
-          Expanded(child: SizedBox(width: 20,height: 30)),
+          Expanded(child: SizedBox(width: 20, height: 30)),
           Text("Author"),
           Text('20241211'),
           Text('top'),
         ],
       )
     ]);
+  }
+
+  Widget _layout() {
+    return Center(
+      child: Text('Some text'),
+    );
   }
 }
